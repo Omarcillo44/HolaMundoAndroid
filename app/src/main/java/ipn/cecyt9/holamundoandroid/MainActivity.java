2c91a3bc7c11d10294dd05205b054d61be647068
Omar Lorenzo Pacheco <omarlorenz44@gmail.com>
2021-11-08 11:52:20 -0600

    Se agregó método, actividad para la suma

--- app/src/main/java/ipn/cecyt9/holamundoandroid/MainActivity.java
+++ app/src/main/java/ipn/cecyt9/holamundoandroid/MainActivity.java
@@ -2,13 +2,39 @@ package ipn.cecyt9.holamundoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void suma(View miView)
+    { }
+    public void resta(View miView)
+    { }
+    public void multiplicacion(View miView)
+    { }
+    public void division(View miView)
+    { }
+
+    private Button Button_suma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button_suma = findViewById(R.id.Button_suma);
+        Button_suma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirSuma();
+            }
+        });
     }
+
+    private void abrirSuma() {
+        Intent intent = new Intent(this , SumaActivity.class);
+    }
+
 }
