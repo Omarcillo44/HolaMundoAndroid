6bef161d56f211f5732fd37377c9ef575a945254
Omar Lorenzo Pacheco <omarlorenz44@gmail.com>
2021-11-11 22:14:22 -0600

    Se agregaron funcionalidades de Resta, División y Multiplicación

--- app/src/main/java/ipn/cecyt9/holamundoandroid/MainActivity.java
+++ app/src/main/java/ipn/cecyt9/holamundoandroid/MainActivity.java
@@ -19,22 +19,66 @@ public class MainActivity extends AppCompatActivity {
     { }
 
     private Button Button_suma;
+    private Button Button_resta;
+    private Button Button_division;
+    private Button Button_multiplicacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button_suma = findViewById(R.id.Button_suma);
+        Button_resta = findViewById(R.id.Button_resta);
+        Button_multiplicacion = findViewById(R.id.Button_multiplicacion);
+        Button_division = findViewById(R.id.Button_division);
+
         Button_suma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 abrirSuma();
             }
         });
+
+        Button_resta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirResta();
+            }
+        });
+
+        Button_multiplicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirMultiplicacion();
+            }
+        });
+
+        Button_division.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirDivision();
+            }
+        });
+
     }
 
     private void abrirSuma() {
         Intent intent = new Intent(this , SumaActivity.class);
     startActivity(intent);
+    }
+
+    private void abrirResta() {
+        Intent intent = new Intent(this , restaActivity.class);
+        startActivity(intent);
+    }
+
+    private void abrirMultiplicacion() {
+        Intent intent = new Intent(this , multiplicacionActivity.class);
+        startActivity(intent);
+    }
+
+    private void abrirDivision() {
+        Intent intent = new Intent(this , divisionActivity.class);
+        startActivity(intent);
     }
 }
